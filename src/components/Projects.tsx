3f5248bbb7e4b200107d8ba6ffcb866cb351810d
bogosorter <luiswbarbosa@gmail.com>
2025-09-03 22:28:59 +0100

    updated project styles and content

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,41 +1,53 @@
+import { Link45deg } from 'react-bootstrap-icons';
 import './Projects.css';
-import mikra from '../images/mikra.png';
-import elementary from '../images/elementary.png';
-import musicly from '../images/musicly.png';
-import flow from '../images/flow.png';
-import typex from '../images/typex.png';
 
 export default function Projects() {
 
     const projects = [
         {
             name: 'bogothoughts',
-            image: mikra,
+            description: 'A cross between a blog and a photo gallery, bogothoughts is a place for whatever comes to mind',
             url: 'https://bogosorter.github.io/blog/'
         }, {
-            name: 'Elementary',
-            image: elementary,
+            name: 'elementary',
+            description: 'A simple and elegant markdown editor',
             url: 'https://bogosorter.github.io/elementary'
         }, {
-            name: 'Musicly',
-            image: musicly,
+            name: 'musicly',
+            description: 'A simple and elegant open-source music player',
             url: 'https://bogosorter.github.io/Musicly'
         }, {
-            name: 'Flowfields',
-            image: flow,
+            name: 'caracol',
+            description: 'My very own slow raytracer',
+            url: 'https://github.com/bogosorter/caracol'
+        }, {
+            name: 'bogosat',
+            description: 'A toy SAT solver that uses gradient ascent',
+            url: 'https://github.com/bogosorter/bogosat'
+        }, {
+            name: 'flowfields',
+            description: 'bogosorter\'s hidden talent for art (and plagiarism)',
             url: 'https://bogosorter.github.io/flow'
         }, {
-            name: 'TypeX',
-            image: typex,
+            name: 'typex',
+            description: 'A website to help you type faster.',
             url: 'https://bogosorter.github.io/TypeX'
+        }, {
+            name: 'colors',
+            description: 'bogosorter\'s favorite color palette',
+            url: 'https://bogosorter.github.io/colors'
+        }, {
+            name: 'periodic',
+            description: 'A relic from the time I had Chemistry',
+            url: 'https://bogosorter.github.io/periodic/'
         }
     ];
 
     return <section id='projects'>
         {projects.map(project => <a href={project.url}>
             <article className='project'>
-                <img src={project.image} alt='' />
-                <h1>{project.name}</h1>
+                <h1><Link45deg /> {project.name}</h1>
+                <p>{project.description}</p>
             </article>
         </a>)}
     </section>
